54d40032a581c6b863836698f47a105e5afd554c
Jasmin Bom <jsmnbom@gmail.com>
2020-09-08 09:23:29 +0200

    Test wallbounce

--- software/main.py
+++ software/main.py
@@ -2,7 +2,7 @@ import serial
 from time import sleep
 import atexit 
 
-from robocar import WallFollowCar
+from robocar import WallFollowCar, WallBounceCar
 
 ser = None
 car = None
@@ -20,11 +20,17 @@ def halt():
 def main():
     global car, ser
     ser = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=0, write_timeout=0)
-    
     sleep(2)
-    car = WallFollowCar(ser)
+
+    # wall follow:
+    # car = WallFollowCar(ser)
+    # car.run()
+
+    # wall bounce:
+    car = WallBounceCar(ser)
     car.run()
 
+    # visualize:
     # visualize = VisualizeData(ser)
     # visualize.run()
 
